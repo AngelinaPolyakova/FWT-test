f4594d575d181224dd249e3fd6335f3beb4d289f
Angelina Polyakova <lina.krasilnikova.1996@mail.ru>
2022-04-26 13:34:37 +0400

    Added button for change the theme

---
+++ src/Component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function Component() {
+  return (
+    <div>
+      <button>Dark/Light</button>
+    </div>
+  );
+}
+
+export default Component;
