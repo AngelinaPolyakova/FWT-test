8e453a448196c8bd2488adbd6f0e0d9cb8fa59b0
Angelina Polyakova <lina.krasilnikova.1996@mail.ru>
2022-04-26 14:34:36 +0400

    Added dark/light themes for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import "./App.css";
-import Component from "./Component";
+
+import { darkTheme, GlobalStyles, lightTheme } from "./themes";
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
-    <div className="App">
-      <Component />
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        Themes on the React
+        <button onClick={() => themeToggler()}>Dark/Light</button>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
