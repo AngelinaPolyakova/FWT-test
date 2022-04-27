c47be372cf1daccf7fbc69148ac997df7d1375c3
Angelina Polyakova <lina.krasilnikova.1996@mail.ru>
2022-04-27 14:51:17 +0400

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./App.css";
+import style from "./App.module.css";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <div className={style.wrapper}>
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-
+import logo from "../img/logo.png";
+import themeToggler from "../img/themeToggler.png";
 import style from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={style.header}>
-      <img src="src\img\Group 2081.png" alt="" />
-      <button>Dark/Light</button>
+      <img className={style.headerLogo} src={logo} alt="" />
+      <a href="#">
+        <img className={style.themeToggler} src={themeToggler} alt="" />
+      </a>
     </header>
   );
 };
